becc06bc15238dff384822ee18cf0f0dc0a2dc5a
Abdelmoughit KINANI <61118207+w1nsid@users.noreply.github.com>
2023-10-01 15:18:49 +0100

    now works proerly

--- classes.py
+++ classes.py
@@ -1,7 +1,7 @@
 import json
 import logging
-import os
 import time
+from pathlib import Path
 
 import ffmpeg
 import numpy as np
@@ -9,8 +9,8 @@ import torch
 import whisper
 import whisper.utils
 
-source_folder = 'video_input'
-output_folder = 'subs_out'
+source_folder = Path('video_input')
+output_folder = Path('subs_out')
 model = 'medium'
 language = 'french'
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -18,11 +18,11 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 #####################################################################
 
 print('Current configuration:')
-print('Source folder: {}'.format(source_folder))
-print('Output folder: {}'.format(output_folder))
-print('Model: {}'.format(model))
-print('Language: {}'.format(language))
-print('Device: {}'.format(device))
+print(f'Source folder: {source_folder}')
+print(f'Output folder: {output_folder}')
+print(f'Model: {model}')
+print(f'Language: {language}')
+print(f'Device: {device}')
 
 
 class Whisperer:
@@ -50,7 +50,7 @@ class Whisperer:
                 language=self.language,
                 verbose=True,
                 word_timestamps=True,
-                initial_prompt=context
+                initial_prompt=context,
             )
         else:
             res = self.model.transcribe(
@@ -62,15 +62,14 @@ class Whisperer:
 
 # Create video class
 class Video:
-    def __init__(self, path, s_out='subs_out', r_out=None, context=None, save_raw=False) -> None:
-        self.path = path
-        self.output_path = s_out
-        self.raw_output_path = r_out
+    def __init__(self, path, s_out=None, r_out=None, context=None, save_raw=False) -> None:
+        self.path: Path = path
+        self.output_path: Path = Path(s_out) if s_out else Path('subs_out')
+        self.raw_output_path: Path = Path(r_out) if r_out else Path('subs_out')
         self.save_raw_flag = save_raw
-        self.path = path
         self.audio_sample_rate = 16000
         self.audio = None
-        self.subtitles: dict[str, str | list]
+        self.subtitles: dict[str, str | list] | None = None
         self.context: str | None = context
 
     def get_audio(self):
@@ -81,12 +80,11 @@ class Video:
                     "-", format="s16le", acodec="pcm_s16le", ac=1, ar=self.audio_sample_rate
                 ).run(cmd=["ffmpeg", "-nostdin"], capture_stdout=True, capture_stderr=True)
             )
+            self.audio = np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0
+            logging.info(f"Loaded audio from {self.path}")
         except ffmpeg.Error as e:
             logging.error(f"Failed to load audio: {e.stderr.decode()}")
             raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
-
-        self.audio = np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0
-        logging.info(f"Loaded audio from {self.path}")
 
     def generate_subtitles(self, transcriber):
         logging.info(f"Generating subtitles for {self.path}")
@@ -107,31 +105,42 @@ class Video:
         if self.subtitles is None:
             logging.error("No subtitles to save")
             raise RuntimeError("No subtitles to save")
-        if self.raw_output_path is None:
-            self.raw_output_path = os.path.join(self.output_path, self.path.replace('.mp4', '.raw'))
-        # create output folder if it doesn't exist
-        os.makedirs(os.path.dirname(self.raw_output_path), exist_ok=True)
-        with open(self.raw_output_path, 'w') as f:
-            # serialize subtitles with json
+
+        raw_output_path = self.raw_output_path / f"{self.path.stem}.json"
+        raw_output_path.parent.mkdir(parents=True, exist_ok=True)
+
+        with raw_output_path.open('w') as f:
             json.dump(self.subtitles, f, indent=4)
         logging.info(f"Saved raw output for {self.path}")
 
     def save_subtitles(self):
         logging.info(f"Saving subtitles for {self.path}")
+        if self.subtitles is None:
+            logging.error("No subtitles to save")
+            raise RuntimeError("No subtitles to save")
 
-        srt_writer = whisper.utils.get_writer("srt", self.output_path)
-        logging.info(f"Saving subtitles for {self.path} to {self.output_path}")
-        # with open(self.output_path, "w", encoding="utf-8") as f:
-        #     srt_writer(self.subtitles, f, {"max_line_width": 47, "max_line_count": 1, "highlight_words": False})
-        srt_writer(
-            self.subtitles,
-            self.output_path,  # type: ignore
-            {
-                "max_line_width": 47,
-                "max_line_count": 1,
-                "highlight_words": False
-            }
-        )
+        output_path = self.output_path
+        output_path.mkdir(parents=True, exist_ok=True)
+
+        srt_writer = whisper.utils.get_writer("srt", str(output_path))
+
+        output_path = self.path.stem + ".srt"
+
+        logging.info(f"Saving subtitles for {self.path} to {output_path}")
+
+        try:
+            srt_writer(
+                self.subtitles,
+                str(output_path),  # type: ignore
+                {
+                    "max_line_width": 47,
+                    "max_line_count": 1,
+                    "highlight_words": False
+                }
+            )
+        except Exception as e:
+            logging.error(f"Failed to save subtitles: {e}")
+            raise RuntimeError(f"Failed to save subtitles: {e}") from e
 
 
 def setup_logging():
@@ -167,31 +176,22 @@ def main():
     whisperer = Whisperer()
     whisperer.init_model()
 
-    # get list of videos in source folder , extension .mp4 mkv
-    videos = [
-        f for f in os.listdir(source_folder)
-        if os.path.isfile(os.path.join(source_folder, f)) and (f.endswith('.mp4') or f.endswith('.mkv'))
-    ]
+    # get list of videos in source folder, extension .mp4 mkv
+    videos = [f for f in source_folder.iterdir() if f.is_file() and f.suffix in ['.mp4', '.mkv']]
     logging.info(f"Found {len(videos)} videos to process")
 
     # read context from file
-    try:
-        with open('context.txt', 'r') as f:
-            context = f.read()
-    except FileNotFoundError:
-        context = None
+    context_file = Path('context.txt')
+    context = context_file.read_text() if context_file.exists() else None
 
     # Process videos
     for video in videos:
         tic = time.time()
-        logging.info(f"Processing video {video}")
-        video_path = os.path.join(source_folder, video)
-        raw_output_path = os.path.join(output_folder, video.replace('.mp4', '.raw'))
-        subs_output_path = os.path.join(output_folder, video.replace('.mp4', '.txt'))
-        video = Video(video_path, save_raw=True, r_out=raw_output_path, s_out=subs_output_path)
-        video.context = context
-        video.generate_subtitles(whisperer)
-        logging.info(f"Processed video {video} in {time.time() - tic:.1f} sec")
+        logging.info(f"Processing video {video.name}")
+        video_obj = Video(video, save_raw=True)
+        video_obj.context = context
+        video_obj.generate_subtitles(whisperer)
+        logging.info(f"Processed video {video.name} in {time.time() - tic:.1f} sec")
 
 
 if __name__ == "__main__":
